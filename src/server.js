e587841662e32000ff52ce15264b4554dfd9af7a
jinhyeok <aqwstn@gmail.com>
2025-02-04 11:11:28 +0900

    Install a morgan

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const PORT = 8000;
+const logger = morgan('dev');
+const home = (req, res) => {
+    res.send("Home");
+}
+const login = (req, res) => res.send("login");
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
