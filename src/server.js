645d720845de59fc847b3433ee97069a7f278f8d
jinhyeok <aqwstn@gmail.com>
2025-02-19 13:06:10 +0900

    Add a express-session to use a session.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const app = express()
+const app = express();
 const logger = morgan("dev");
 app.use(logger);
+
+app.use(
+  session({
+    secret: "hello",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
@@ -16,4 +25,3 @@ app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
 export default app;
-
